2bd147905502b472ff4b5a9dcb39c62e610643cf
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2022-08-31 19:22:15 +0530

    Resolved setAuthHeaders is not defined

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 
+import { setAuthHeaders } from "apis/axios";
 import { initializeLogger } from "common/logger";
 
 const App = () => {
