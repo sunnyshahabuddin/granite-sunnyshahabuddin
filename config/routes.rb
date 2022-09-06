f587811e7bbb9c3fffcecb34144722b508e771ce
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2022-09-06 21:41:53 +0530

    Added ability to assign task to a user

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@
 
 Rails.application.routes.draw do
   resources :tasks, except: %i[new edit], param: :slug
+  resources :users, only: :index
+
   root "home#index"
   get "*path", to: "home#index", via: :all
 
