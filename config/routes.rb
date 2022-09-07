69d87b8f4903fb9b8d38a607ad904291986b2345
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2022-09-07 10:36:57 +0530

    Added Jbuilder template for show action in TasksController

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,28 @@
 # frozen_string_literal: true
 
+# Rails.application.routes.draw do
+#   # resources :tasks, except: %i[new edit], param: :slug, defaults: { format: "json" }
+#   # resources :users, only: :index
+#   defaults format: :json do
+#     resources :tasks, except: %i[new edit], param: :slug
+#     resources :users, only: :index
+#   end
+
+#   root "home#index"
+#   get "*path", to: "home#index", via: :all
+
+# end
+
 Rails.application.routes.draw do
-  resources :tasks, except: %i[new edit], param: :slug
-  resources :users, only: :index
+  constraints(lambda { |req| req.format == :json }) do
+    resources :tasks, except: %i[new edit], param: :slug
+    resources :users, only: :index
+  end
 
   root "home#index"
   get "*path", to: "home#index", via: :all
-
 end
+
 # Rails.application.routes.draw do
 #   resources :tasks, only: %i[index create], param: :slug
 # end
