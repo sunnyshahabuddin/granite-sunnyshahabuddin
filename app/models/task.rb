540037ce7da5a675f8afa5459b5e8a87b2b44a57
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2022-09-09 10:06:10 +0530

    Added progress to tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
+  RESTRICTED_ATTRIBUTES = %i[title task_owner_id assigned_user_id]
   belongs_to :assigned_user, foreign_key: "assigned_user_id", class_name: "User"
   belongs_to :task_owner, foreign_key: "task_owner_id", class_name: "User"
   validates :title, presence: true, length: { maximum: 50 }
   validates :slug, uniqueness: true
   validate :slug_not_changed
   has_many :comments, dependent: :destroy
+
+  enum progress: { pending: "pending", completed: "completed" }
 
   before_create :set_slug
 
